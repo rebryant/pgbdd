47be890010e91f7b7e2da18a994cc42f496c8921
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-05-11 09:55:56 -0400

    Script to generate Xor benchmarks

---
+++ benchmarks/xor-data/gen-lp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+make N=010
+make N=020
+make N=030
+make N=040
+make N=050
+make N=060
+make N=070
+make N=080
+make N=090
+make N=100
+make N=110
+make N=120
+make N=130
+make N=140
+make N=150
+make N=160
+make N=170
+make N=180
+make N=190
+make N=200
+make N=210
+make N=220
+make N=230
+make N=240
+make N=250
+make N=260
+make N=270
+make N=280
+make N=290
+make N=300
+make N=310
+make N=320
