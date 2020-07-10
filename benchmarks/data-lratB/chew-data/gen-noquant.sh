0502945d9076da0ef3ba3856fa9ae155686c11f3
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-07-10 12:34:42 -0400

    New benchmark

---
+++ benchmarks/data-lratB/chew-data/gen-noquant.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make nq N=00004
+make nq N=00008
+make nq N=00012
+make nq N=00014
+make nq N=00016
+make nq N=00018
+make nq N=00019
+make nq N=00020
+make nq N=00021
+make nq N=00022
+make nq N=00023
+make nq N=00024
+#make nq N=00032
