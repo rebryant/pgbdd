78c0bc158f78a7bee24bb0410bfd7e1244796ad0
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-08-03 15:22:40 -0400

    Benchmarks for fixed-point proof

---
+++ benchmarks/data-lrat-induct/chess-data/gen-chess.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+make q N=008 M=006
+make q N=016 M=010
+make q N=024 M=014
+make q N=032 M=018
+make q N=040 M=022
+make q N=048 M=026
+make q N=064 M=034
+make q N=080 M=042
+make q N=096 M=050
+make q N=112 M=058
+make q N=128 M=066
+make q N=144 M=074
+make q N=160 M=082
+make q N=176 M=090
+make q N=192 M=098
+make q N=200 M=102
+
+
+
