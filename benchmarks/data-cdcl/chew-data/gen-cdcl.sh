ac9c1e99165fdf835d0ad6c81e02e50a565604a9
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-08-14 15:12:18 -0400

    More benchmarks

---
+++ benchmarks/data-cdcl/chew-data/gen-cdcl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+make c N=00004
+make c N=00008
+make c N=00012
+make c N=00014
+make c N=00016
+make c N=00018
+make c N=00019
+make c N=00020
+make c N=00021
+make c N=00022
+make c N=00023
+make c N=00024
+make c N=00025
+make c N=00026
+make c N=00027
+make c N=00028
+#make c N=00032
