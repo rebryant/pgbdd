f746beadb31b9cb5ecd198a06b35798ba775fda1
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-08-14 14:48:29 -0400

    Finished benchmarking

--- benchmarks/data-cdcl/pigeon-tseitin-data/gen-pigeon-cdcl.sh
+++ benchmarks/data-cdcl/pigeon-tseitin-data/gen-pigeon-cdcl.sh
@@ -3,17 +3,11 @@ make c N=004
 make c N=006
 make c N=008
 make c N=010
+make c N=011
 make c N=012
+make c N=013
 make c N=014
-make c N=016
-make c N=018
-make c N=020
-make c N=022
-make c N=024
-make c N=026
-make c N=028
-make c N=030
-
-
-
-
+make c N=015
+#make c N=016
+#make c N=017
+#make c N=018
