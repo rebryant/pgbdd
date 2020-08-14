0741aee270406a4110764e5719bcd7c65176c365
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-08-14 14:58:14 -0400

    New benchmarks

---
+++ benchmarks/data-lrat/urquhart-li-data/gen-linear.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+make l M=02
+make l M=03
+make l M=04
---
+++ benchmarks/data-lrat/urquhart-li-data/gen-randomorder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+make ro M=02
+make ro M=03
+make ro M=04
+make ro M=06
+make ro M=08
+make ro M=10
+make ro M=12
+make ro M=16
+make ro M=20
+make ro M=22
+make ro M=24
+make ro M=28
+
