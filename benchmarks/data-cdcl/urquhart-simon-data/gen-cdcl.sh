866905784f9001f8ffa629825cea88d3d0d953f1
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-08-14 22:31:08 -0400

    Added benchmarks

---
+++ benchmarks/data-cdcl/urquhart-simon-data/gen-cdcl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+make c M=03 SEED=01
+make c M=03 SEED=02
+make c M=03 SEED=03
+make c M=03 SEED=04
+make c M=03 SEED=05
+make c M=04 SEED=01
+make c M=04 SEED=02
+make c M=04 SEED=03
+make c M=04 SEED=04
+make c M=04 SEED=05
+# make c M=05 SEED=01
+# make c M=05 SEED=02
+# make c M=05 SEED=03
+# make c M=05 SEED=04
+# make c M=05 SEED=05
