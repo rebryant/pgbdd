d417ac412326e62bf9c581e23cd3f117c45371a8
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-11-17 16:50:43 -0500

    Benchmark Execution for Kleine-Buening Formulas

---
+++ qbenchmarks/data-qproof/kbf/gen.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+make vo T=00010
+make vo T=00020
+make vo T=00050
+make vo T=00100
+make vo T=00200
+#make vo T=00500
+#make vo T=01000
+
+
+make rvo T=00010
+make rvo T=00020
+make rvo T=00050
+make rvo T=00100
+make rvo T=00200
+make rvo T=00500
+make rvo T=01000
+make rvo T=02000
+make rvo T=05000
+#make rvo T=10000
+
+make qo T=00010
+make qo T=00020
+make qo T=00050
+make qo T=00100
+make qo T=00200
+#make qo T=00500
+#make qo T=01000
+
+
+make rqo T=00010
+make rqo T=00020
+make rqo T=00050
+make rqo T=00100
+make rqo T=00200
+make rqo T=00500
+make rqo T=01000
+make rqo T=02000
+make rqo T=05000
+#make rqo T=10000
+
+
