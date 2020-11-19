3f91171818cd49a5c0fc9fe1e389c42e0ced4ccc
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-11-19 10:49:20 -0500

    Define benchmarks for linear performance

--- qbenchmarks/data-qproof/kbf/gen.sh
+++ qbenchmarks/data-qproof/kbf/gen.sh
@@ -1,96 +1,82 @@
 #!/bin/bash
 
-make ran SEED=1 T=00010
-make ran SEED=1 T=00020
-make ran SEED=1 T=00050
-make ran SEED=1 T=00100
-make ran SEED=1 T=00200
 make ran SEED=1 T=00500
 make ran SEED=1 T=01000
+make ran SEED=1 T=01500
 make ran SEED=1 T=02000
+make ran SEED=1 T=02500
+make ran SEED=1 T=03000
+make ran SEED=1 T=03500
+make ran SEED=1 T=04000
+make ran SEED=1 T=04500
 make ran SEED=1 T=05000
 
-make ran SEED=2 T=00010
-make ran SEED=2 T=00020
-make ran SEED=2 T=00050
-make ran SEED=2 T=00100
-make ran SEED=2 T=00200
 make ran SEED=2 T=00500
 make ran SEED=2 T=01000
+make ran SEED=2 T=01500
 make ran SEED=2 T=02000
+make ran SEED=2 T=02500
+make ran SEED=2 T=03000
+make ran SEED=2 T=03500
+make ran SEED=2 T=04000
+make ran SEED=2 T=04500
 make ran SEED=2 T=05000
 
-make ran SEED=3 T=00010
-make ran SEED=3 T=00020
-make ran SEED=3 T=00050
-make ran SEED=3 T=00100
-make ran SEED=3 T=00200
 make ran SEED=3 T=00500
 make ran SEED=3 T=01000
+make ran SEED=3 T=01500
 make ran SEED=3 T=02000
+make ran SEED=3 T=02500
+make ran SEED=3 T=03000
+make ran SEED=3 T=03500
+make ran SEED=3 T=04000
+make ran SEED=3 T=04500
 make ran SEED=3 T=05000
 
-make ran SEED=4 T=00010
-make ran SEED=4 T=00020
-make ran SEED=4 T=00050
-make ran SEED=4 T=00100
-make ran SEED=4 T=00200
 make ran SEED=4 T=00500
 make ran SEED=4 T=01000
+make ran SEED=4 T=01500
 make ran SEED=4 T=02000
+make ran SEED=4 T=02500
+make ran SEED=4 T=03000
+make ran SEED=4 T=03500
+make ran SEED=4 T=04000
+make ran SEED=4 T=04500
 make ran SEED=4 T=05000
 
-make ran SEED=5 T=00010
-make ran SEED=5 T=00020
-make ran SEED=5 T=00050
-make ran SEED=5 T=00100
-make ran SEED=5 T=00200
 make ran SEED=5 T=00500
 make ran SEED=5 T=01000
+make ran SEED=5 T=01500
 make ran SEED=5 T=02000
+make ran SEED=5 T=02500
+make ran SEED=5 T=03000
+make ran SEED=5 T=03500
+make ran SEED=5 T=04000
+make ran SEED=5 T=04500
 make ran SEED=5 T=05000
 
-
-make vo T=00010
-make vo T=00020
-make vo T=00050
-make vo T=00100
-make vo T=00200
-make vo T=00500
-make vo T=01000
-make vo T=02000
-make vo T=05000
-
-make rvo T=00010
-make rvo T=00020
-make rvo T=00050
-make rvo T=00100
-make rvo T=00200
 make rvo T=00500
 make rvo T=01000
+make rvo T=01500
 make rvo T=02000
+make rvo T=02500
+make rvo T=03000
+make rvo T=03500
+make rvo T=04000
+make rvo T=04500
 make rvo T=05000
-make rvo T=10000
 
-make qo T=00010
-make qo T=00020
-make qo T=00050
-make qo T=00100
-make qo T=00200
-make qo T=00500
-make qo T=01000
-make qo T=02000
-make qo T=05000
-
-make rqo T=00010
-make rqo T=00020
-make rqo T=00050
-make rqo T=00100
-make rqo T=00200
 make rqo T=00500
 make rqo T=01000
+make rqo T=01500
 make rqo T=02000
+make rqo T=02500
+make rqo T=03000
+make rqo T=03500
+make rqo T=04000
+make rqo T=04500
 make rqo T=05000
-#make rqo T=10000
+
+
 
 
