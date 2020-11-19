49860d95df1f1257dd1d50f638b1600f080626b3
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-11-18 21:20:33 -0500

    Added random benchmarks

--- qbenchmarks/data-qproof/kbf/gen.sh
+++ qbenchmarks/data-qproof/kbf/gen.sh
@@ -1,13 +1,65 @@
 #!/bin/bash
 
+make ran SEED=1 T=00010
+make ran SEED=1 T=00020
+make ran SEED=1 T=00050
+make ran SEED=1 T=00100
+make ran SEED=1 T=00200
+make ran SEED=1 T=00500
+make ran SEED=1 T=01000
+make ran SEED=1 T=02000
+make ran SEED=1 T=05000
+
+make ran SEED=2 T=00010
+make ran SEED=2 T=00020
+make ran SEED=2 T=00050
+make ran SEED=2 T=00100
+make ran SEED=2 T=00200
+make ran SEED=2 T=00500
+make ran SEED=2 T=01000
+make ran SEED=2 T=02000
+make ran SEED=2 T=05000
+
+make ran SEED=3 T=00010
+make ran SEED=3 T=00020
+make ran SEED=3 T=00050
+make ran SEED=3 T=00100
+make ran SEED=3 T=00200
+make ran SEED=3 T=00500
+make ran SEED=3 T=01000
+make ran SEED=3 T=02000
+make ran SEED=3 T=05000
+
+make ran SEED=4 T=00010
+make ran SEED=4 T=00020
+make ran SEED=4 T=00050
+make ran SEED=4 T=00100
+make ran SEED=4 T=00200
+make ran SEED=4 T=00500
+make ran SEED=4 T=01000
+make ran SEED=4 T=02000
+make ran SEED=4 T=05000
+
+make ran SEED=5 T=00010
+make ran SEED=5 T=00020
+make ran SEED=5 T=00050
+make ran SEED=5 T=00100
+make ran SEED=5 T=00200
+make ran SEED=5 T=00500
+make ran SEED=5 T=01000
+make ran SEED=5 T=02000
+make ran SEED=5 T=05000
+
+
 make vo T=00010
 make vo T=00020
 make vo T=00050
 make vo T=00100
 make vo T=00200
-#make vo T=00500
-#make vo T=01000
-
+make vo T=00500
+make vo T=01000
+make vo T=02000
+make vo T=05000
 
 make rvo T=00010
 make rvo T=00020
@@ -18,16 +70,17 @@ make rvo T=00500
 make rvo T=01000
 make rvo T=02000
 make rvo T=05000
-#make rvo T=10000
+make rvo T=10000
 
 make qo T=00010
 make qo T=00020
 make qo T=00050
 make qo T=00100
 make qo T=00200
-#make qo T=00500
-#make qo T=01000
-
+make qo T=00500
+make qo T=01000
+make qo T=02000
+make qo T=05000
 
 make rqo T=00010
 make rqo T=00020
