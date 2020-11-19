145e586b706239d4c7aa7de70854a8cb97f2c748
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-11-19 14:07:11 -0500

    Add more random cases

--- qbenchmarks/data-qproof/kbf/gen.sh
+++ qbenchmarks/data-qproof/kbf/gen.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+make ran SEED=0 T=00050
+make ran SEED=0 T=00500
+make ran SEED=0 T=01000
+make ran SEED=0 T=01500
+make ran SEED=0 T=02000
+make ran SEED=0 T=02500
+make ran SEED=0 T=03000
+make ran SEED=0 T=03500
+make ran SEED=0 T=04000
+make ran SEED=0 T=04500
+make ran SEED=0 T=05000
+
+make ran SEED=1 T=00050
 make ran SEED=1 T=00500
 make ran SEED=1 T=01000
 make ran SEED=1 T=01500
@@ -11,6 +24,7 @@ make ran SEED=1 T=04000
 make ran SEED=1 T=04500
 make ran SEED=1 T=05000
 
+make ran SEED=2 T=00050
 make ran SEED=2 T=00500
 make ran SEED=2 T=01000
 make ran SEED=2 T=01500
@@ -22,6 +36,7 @@ make ran SEED=2 T=04000
 make ran SEED=2 T=04500
 make ran SEED=2 T=05000
 
+make ran SEED=2 T=00050
 make ran SEED=3 T=00500
 make ran SEED=3 T=01000
 make ran SEED=3 T=01500
@@ -33,6 +48,7 @@ make ran SEED=3 T=04000
 make ran SEED=3 T=04500
 make ran SEED=3 T=05000
 
+make ran SEED=4 T=00050
 make ran SEED=4 T=00500
 make ran SEED=4 T=01000
 make ran SEED=4 T=01500
@@ -44,6 +60,7 @@ make ran SEED=4 T=04000
 make ran SEED=4 T=04500
 make ran SEED=4 T=05000
 
+make ran SEED=5 T=00050
 make ran SEED=5 T=00500
 make ran SEED=5 T=01000
 make ran SEED=5 T=01500
@@ -55,6 +72,55 @@ make ran SEED=5 T=04000
 make ran SEED=5 T=04500
 make ran SEED=5 T=05000
 
+make ran SEED=6 T=00050
+make ran SEED=6 T=00500
+make ran SEED=6 T=01000
+make ran SEED=6 T=01500
+make ran SEED=6 T=02000
+make ran SEED=6 T=02500
+make ran SEED=6 T=03000
+make ran SEED=6 T=03500
+make ran SEED=6 T=04000
+make ran SEED=6 T=04500
+make ran SEED=6 T=05000
+
+make ran SEED=7 T=00050
+make ran SEED=7 T=00500
+make ran SEED=7 T=01000
+make ran SEED=7 T=01500
+make ran SEED=7 T=02000
+make ran SEED=7 T=02500
+make ran SEED=7 T=03000
+make ran SEED=7 T=03500
+make ran SEED=7 T=04000
+make ran SEED=7 T=04500
+make ran SEED=7 T=05000
+
+make ran SEED=8 T=00050
+make ran SEED=8 T=00500
+make ran SEED=8 T=01000
+make ran SEED=8 T=01500
+make ran SEED=8 T=02000
+make ran SEED=8 T=02500
+make ran SEED=8 T=03000
+make ran SEED=8 T=03500
+make ran SEED=8 T=04000
+make ran SEED=8 T=04500
+make ran SEED=8 T=05000
+
+make ran SEED=9 T=00050
+make ran SEED=9 T=00500
+make ran SEED=9 T=01000
+make ran SEED=9 T=01500
+make ran SEED=9 T=02000
+make ran SEED=9 T=02500
+make ran SEED=9 T=03000
+make ran SEED=9 T=03500
+make ran SEED=9 T=04000
+make ran SEED=9 T=04500
+make ran SEED=9 T=05000
+
+make rvo T=00050
 make rvo T=00500
 make rvo T=01000
 make rvo T=01500
@@ -66,6 +132,7 @@ make rvo T=04000
 make rvo T=04500
 make rvo T=05000
 
+make rqo T=00050
 make rqo T=00500
 make rqo T=01000
 make rqo T=01500
