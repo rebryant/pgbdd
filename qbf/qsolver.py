5983fea1c397c2076e751a9197be2b0995880e2f
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2020-11-19 21:30:08 -0500

    Moved Permuter class to separate file

--- qbf/qsolver.py
+++ qbf/qsolver.py
@@ -9,6 +9,7 @@ import bdd
 import resolver
 import stream
 import qreader
+import permutation
 
 # Increase maximum recursion depth
 sys.setrecursionlimit(50 * sys.getrecursionlimit())
@@ -106,63 +107,6 @@ class Term:
         return Term(self.manager, newRoot, validation)
 
 
-class PermutationException(Exception):
-
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return "Permutation Exception: " + str(self.value)
-
-
-class Permuter:
-    forwardMap = {}
-    reverseMap = {}
-    
-    def __init__(self, valueList = [], permutedList = []):
-        self.forwardMap = {}
-        self.reverseMap = {}
-        identity = False
-        if len(permutedList) == 0:
-            permutedList = valueList
-            identity = True
-        if len(valueList) != len(permutedList):
-            raise PermutationException("Unequal list lengths: %d, %d" % (len(valueList), len(permutedList)))
-        for v, p in zip(valueList, permutedList):
-            self.forwardMap[v] = p
-            self.reverseMap[p] = v
-        if identity:
-            return
-        # Check permutation
-        for v in valueList:
-            if v not in self.reverseMap:
-                raise PermutationException("Not permutation: Nothing maps to %s" % str(v))
-        for v in permutedList:
-            if v not in self.forwardMap:
-                raise PermutationException("Not permutation: %s does not map anything" % str(v))
-            
-            
-    def forward(self, v):
-        if v not in self.forwardMap:
-            raise PermutationException("Value %s not in permutation" % (str(v)))
-        return self.forwardMap[v]
-
-    def permLess(self, v1, v2):
-        pv1 = self.reverse(v1)
-        pv2 = self.reverse(v2)
-        return pv1 < pv2
-
-    def sortList(self, ls):
-        return sorted(ls, key = lambda x: self.reverse(x))
-
-
-    def reverse(self, v):
-        if v not in self.reverseMap:
-            raise PermutationException("Value %s not in permutation range" % (str(v)))
-        return self.reverseMap[v]
-    
-    def __len__(self):
-        return len(self.forwardMap)
         
 class ProverException(Exception):
 
@@ -410,7 +354,7 @@ class Solver:
         # Generate BDD representations of literals
         if permuter is None:
             # Default is identity permutation
-            permuter = Permuter(list(range(1, reader.nvar+1)))
+            permuter = permutation.Permuter(list(range(1, reader.nvar+1)))
         self.permuter = permuter
         # Construct literal map
         self.litMap = {}
@@ -633,7 +577,7 @@ def readPermutation(fname, writer = None):
             permutedList.append(v)
     infile.close()
     try:
-        permuter = Permuter(valueList, permutedList)
+        permuter = permutation.Permuter(valueList, permutedList)
     except Exception as ex:
         writer.write("Invalid permutation: %s\n" % str(ex))
         return None
