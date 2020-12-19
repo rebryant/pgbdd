8e7188b9df12b1022b77467be52f5b58151f5ce3
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-12-19 15:19:34 -0500

    Split into balanced and skewed benchmarks

---
+++ qbenchmarks/data-qproof/nim/gen-b2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+make orab CAT='-b2' PROFILE=1+1
+make orab CAT='-b2' PROFILE=2+2
+make orab CAT='-b2' PROFILE=3+3
+make orab CAT='-b2' PROFILE=4+4
+make orab CAT='-b2' PROFILE=5+5
+make orab CAT='-b2' PROFILE=6+6
+make orab CAT='-b2' PROFILE=7+7
+make orab CAT='-b2' PROFILE=8+8
+make orab CAT='-b2' PROFILE=9+9
+make orab CAT='-b2' PROFILE=10+10
---
+++ qbenchmarks/data-qproof/nim/gen-b5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+make osab CAT='-b5' PROFILE=1+1+1+1+1
+make osab CAT='-b5' PROFILE=1+1+1+2+2
+make osab CAT='-b5' PROFILE=2+2+2+2+2
+make osab CAT='-b5' PROFILE=2+2+2+3+3
+make osab CAT='-b5' PROFILE=3+3+3+3+3
+make osab CAT='-b5' PROFILE=3+3+3+4+4
+make osab CAT='-b5' PROFILE=4+4+4+4+4
+make osab CAT='-b5' PROFILE=4+4+4+5+5
+make osab CAT='-b5' PROFILE=4+4+5+5+5
+make osab CAT='-b5' PROFILE=4+5+5+5+5
+make osab CAT='-b5' PROFILE=5+5+5+5+5
+
