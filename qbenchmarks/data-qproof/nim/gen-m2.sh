7ff801a306c0fe8bf43559580a4a95febc9dbb34
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-12-21 09:01:51 -0500

    Updated benchmarks

---
+++ qbenchmarks/data-qproof/nim/gen-m2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+make osab CAT='-m2' PROFILE=1+10
+make osab CAT='-m2' PROFILE=2+10
+make osab CAT='-m2' PROFILE=3+10
+make osab CAT='-m2' PROFILE=4+10
+make osab CAT='-m2' PROFILE=5+10
+make osab CAT='-m2' PROFILE=6+10
+make osab CAT='-m2' PROFILE=7+10
+make osab CAT='-m2' PROFILE=8+10
+make osab CAT='-m2' PROFILE=9+10
+make orab CAT='-m2' PROFILE=10+10
