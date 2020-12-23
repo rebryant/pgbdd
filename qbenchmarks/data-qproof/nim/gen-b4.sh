d4799c96dce5222581deb3d6ce9a56da6e8feba3
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-12-23 16:33:55 -0500

    Update benchmarks

--- qbenchmarks/data-qproof/nim/gen-b4.sh
+++ qbenchmarks/data-qproof/nim/gen-b4.sh
@@ -4,3 +4,5 @@ make orab CAT='-b4' PROFILE=2+2+2+2
 make orab CAT='-b4' PROFILE=3+3+3+3
 make orab CAT='-b4' PROFILE=4+4+4+4
 make orab CAT='-b4' PROFILE=5+5+5+5
+make orab CAT='-b4' PROFILE=6+6+6+6
+make orab CAT='-b4' PROFILE=7+7+7+7
---
+++ qbenchmarks/data-qproof/nim/gen-tf4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+make orab CAT='-f4' PROFILE=2+2+3+3
+make osab CAT='-t4' PROFILE=1+3+3+3
+make orab CAT='-f4' PROFILE=3+3+4+4
+make osab CAT='-t4' PROFILE=2+4+4+4
+make orab CAT='-f4' PROFILE=4+4+5+5
+make osab CAT='-t4' PROFILE=3+5+5+5
+make orab CAT='-f4' PROFILE=5+5+6+6
+make osab CAT='-t4' PROFILE=4+6+6+6
+make orab CAT='-f4' PROFILE=6+6+7+7
+#make osab CAT='-t4' PROFILE=5+7+7+7
+make orab CAT='-f4' PROFILE=7+7+8+8
+#make osab CAT='-t4' PROFILE=6+8+8+8
