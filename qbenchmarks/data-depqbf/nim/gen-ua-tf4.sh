8e85d948cf09d42212cf6fd884a0fef210a6bd5f
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2020-12-24 14:13:15 -0500

    More benchmarks

---
+++ qbenchmarks/data-depqbf/nim/gen-ua-tf4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+make ua CAT='-f4' PROFILE=2+2+3+3
+make ua CAT='-t4' PROFILE=1+3+3+3
+make ua CAT='-f4' PROFILE=3+3+4+4
+make ua CAT='-t4' PROFILE=2+4+4+4
+make ua CAT='-f4' PROFILE=4+4+5+5
+make ua CAT='-t4' PROFILE=3+5+5+5
+make ua CAT='-f4' PROFILE=5+5+6+6
+make ua CAT='-t4' PROFILE=4+6+6+6
+make ua CAT='-f4' PROFILE=6+6+7+7
+make ua CAT='-t4' PROFILE=5+7+7+7
+make ua CAT='-f4' PROFILE=7+7+8+8
+make ua CAT='-t4' PROFILE=6+8+8+8
