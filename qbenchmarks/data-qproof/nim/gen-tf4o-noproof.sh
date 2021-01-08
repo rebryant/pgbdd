b2bd585259b88f29000d40caf3dfe9fb937e5908
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-01-08 18:34:03 -0500

    Added no-proof benchmarks

---
+++ qbenchmarks/data-qproof/nim/gen-tf4o-noproof.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+make onao CAT='-f4' PROFILE=1+1+1+1
+make onao CAT='-t4' PROFILE=1+1+1+2
+make onao CAT='-f4' PROFILE=1+1+2+2
+make onao CAT='-t4' PROFILE=1+2+2+2
+make onao CAT='-f4' PROFILE=2+2+2+2
+make onao CAT='-t4' PROFILE=2+3+3+3
+make onao CAT='-f4' PROFILE=2+2+3+3
+make onao CAT='-t4' PROFILE=1+3+3+3
+make onao CAT='-f4' PROFILE=3+3+4+4
+make onao CAT='-t4' PROFILE=2+4+4+4
+make onao CAT='-f4' PROFILE=4+4+5+5
+make onao CAT='-t4' PROFILE=3+5+5+5
+# 19
+make onao CAT='-t4' PROFILE=4+5+5+5
+# 20
+make onao CAT='-f4' PROFILE=5+5+5+5
+make onao CAT='-t4' PROFILE=4+5+5+6
+# 21
+make onao CAT='-t4' PROFILE=5+5+5+6
+make onao CAT='-f4' PROFILE=5+5+6+6
+make onao CAT='-t4' PROFILE=4+6+6+6
+# 23
+make onao CAT='-t4' PROFILE=5+6+6+6
+# 24
+make onao CAT='-f4' PROFILE=6+6+6+6
+make onao CAT='-f4' PROFILE=6+6+7+7
+make onao CAT='-t4' PROFILE=5+7+7+7
+# 28
+make onao CAT='-f4' PROFILE=7+7+7+7
+make onao CAT='-f4' PROFILE=7+7+8+8
+make onao CAT='-t4' PROFILE=6+8+8+8
+# 32
+make onao CAT='-f4' PROFILE=8+8+8+8
+# 34
+make onao CAT='-f4' PROFILE=8+8+9+9
+make onao CAT='-f4' PROFILE=9+9+9+9
+make onao CAT='-f4' PROFILE=10+10+10+10
+make onao CAT='-f4' PROFILE=11+11+11+11
+make onao CAT='-f4' PROFILE=12+12+12+12
+
