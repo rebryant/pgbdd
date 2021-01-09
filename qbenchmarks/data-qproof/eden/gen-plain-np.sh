b05034f6cec5a453527c15a8385c70cdb05dc510
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2021-01-08 22:30:50 -0500

    No-proof benchmarks

---
+++ qbenchmarks/data-qproof/eden/gen-plain-np.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Little
+make np MODE=plain R=01 C=01
+make np MODE=plain R=01 C=02
+make np MODE=plain R=02 C=01
+make np MODE=plain R=01 C=03
+make np MODE=plain R=03 C=01
+# 4
+make np MODE=plain R=01 C=04
+make np MODE=plain R=02 C=02
+make np MODE=plain R=04 C=01
+# 5
+make np MODE=plain R=01 C=05
+make np MODE=plain R=05 C=01
+# 6
+make np MODE=plain R=01 C=06
+make np MODE=plain R=02 C=03
+make np MODE=plain R=03 C=02
+make np MODE=plain R=06 C=01
+# 7
+make np MODE=plain R=01 C=07
+make np MODE=plain R=07 C=01
+# 8
+make np MODE=plain R=01 C=08
+make np MODE=plain R=02 C=04
+make np MODE=plain R=04 C=02
+make np MODE=plain R=08 C=01
+# 9
+make np MODE=plain R=01 C=09
+make np MODE=plain R=03 C=03
+make np MODE=plain R=09 C=01
+# 10
+make np MODE=plain R=01 C=10
+make np MODE=plain R=02 C=05
+make np MODE=plain R=05 C=02
+make np MODE=plain R=10 C=01
+# 11
+make np MODE=plain R=01 C=11
+make np MODE=plain R=11 C=01
+# 12
+make np MODE=plain R=01 C=12
+make np MODE=plain R=02 C=06
+make np MODE=plain R=03 C=04
+make np MODE=plain R=04 C=03
+make np MODE=plain R=06 C=02
+make np MODE=plain R=12 C=01
+# 13
+make np MODE=plain R=01 C=13
+make np MODE=plain R=13 C=01
+# 14
+make np MODE=plain R=01 C=14
+make np MODE=plain R=02 C=07
+make np MODE=plain R=14 C=01
+# 15
+make np MODE=plain R=01 C=15
+make np MODE=plain R=03 C=05
+make np MODE=plain R=15 C=01
+# 16
+make np MODE=plain R=01 C=16
+make np MODE=plain R=02 C=08
+#make np MODE=plain R=04 C=04
+make np MODE=plain R=16 C=01
+
+# Big ones
+make np MODE=plain R=07 C=02
+make np MODE=plain R=05 C=03
+#make np MODE=plain R=08 C=02
+
+# Slender
+make np MODE=plain R=01 C=18
+make np MODE=plain R=01 C=20
+make np MODE=plain R=02 C=09
+make np MODE=plain R=02 C=10
+
+make np MODE=plain R=01 C=22
+make np MODE=plain R=02 C=11
+
+make np MODE=plain R=01 C=24
+make np MODE=plain R=02 C=12
+make np MODE=plain R=01 C=26
+make np MODE=plain R=01 C=28
+make np MODE=plain R=01 C=30
+
+make np MODE=plain R=02 C=13
+make np MODE=plain R=02 C=14
+make np MODE=plain R=02 C=15
+
+make np MODE=plain R=18 C=01
+make np MODE=plain R=20 C=01
+make np MODE=plain R=22 C=01
+make np MODE=plain R=24 C=01
+make np MODE=plain R=26 C=01
+make np MODE=plain R=28 C=01
+make np MODE=plain R=30 C=01
+
+# Harder
+make np MODE=plain R=03 C=06
