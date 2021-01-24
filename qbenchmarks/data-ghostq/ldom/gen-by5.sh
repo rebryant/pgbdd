28cefaa2fea227bd3557a8c5abc73dc14024ec0f
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-01-24 17:07:12 -0500

    Created uniform set of benchmarks

---
+++ qbenchmarks/data-ghostq/ldom/gen-by5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+make a CAT=-BY5 N=05
+make a CAT=-BY5 N=10
+make a CAT=-BY5 N=15
+make a CAT=-BY5 N=20
+make a CAT=-BY5 N=25
+make a CAT=-BY5 N=30
---
+++ qbenchmarks/data-qproof/ldom/gen-by5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+make ra CAT=-BY5 N=05
+make sa CAT=-BY5 N=10
+make ra CAT=-BY5 N=15
+make sa CAT=-BY5 N=20
+make ra CAT=-BY5 N=25
+make sa CAT=-BY5 N=30
