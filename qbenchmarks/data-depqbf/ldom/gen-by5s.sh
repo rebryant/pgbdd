605a25ea3cdb1cf563c42643ec8cb8f323c90255
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-01-25 18:37:57 -0500

    Update benchmarks

--- qbenchmarks/data-depqbf/ldom/gen-by5s.sh
+++ qbenchmarks/data-depqbf/ldom/gen-by5s.sh
@@ -4,5 +4,5 @@ make ia CAT=-BY5S N=05
 make a CAT=-BY5S N=10
 make ia CAT=-BY5S N=15
 make a CAT=-BY5S N=20
-make ia CAT=-BY5S N=25
-make a CAT=-BY5S N=30
+#make ia CAT=-BY5S N=25
+#make a CAT=-BY5S N=30
