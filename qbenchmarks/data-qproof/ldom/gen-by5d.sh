0ea28d9e20060594f09911be680d3b3cff18fbec
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-02-02 16:25:54 -0500

    Enable full set of tests

--- qbenchmarks/data-qproof/ldom/gen-by5d.sh
+++ qbenchmarks/data-qproof/ldom/gen-by5d.sh
@@ -6,9 +6,9 @@ make da CAT=-BY5DS N=10
 make ida CAT=-BY5DR N=10
 make da CAT=-BY5DR N=15
 make ida CAT=-BY5DS N=15
-# make da CAT=-BY5DS N=20
-# make ida CAT=-BY5DR N=20
-# make da CAT=-BY5DR N=25
-# make ida CAT=-BY5DS N=25
-# make da CAT=-BY5DS N=30
-# make ida CAT=-BY5DR N=30
+make da CAT=-BY5DS N=20
+make ida CAT=-BY5DR N=20
+make da CAT=-BY5DR N=25
+make ida CAT=-BY5DS N=25
+make da CAT=-BY5DS N=30
+make ida CAT=-BY5DR N=30
