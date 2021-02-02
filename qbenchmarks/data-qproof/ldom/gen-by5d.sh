d53878c57f9383ce3838f13802ce2b42bb6a82e0
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-02-02 16:25:21 -0500

    Dual benchmarks

---
+++ qbenchmarks/data-qproof/ldom/gen-by5d.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+make da CAT=-BY5DR N=05
+make ida CAT=-BY5DS N=05
+make da CAT=-BY5DS N=10
+make ida CAT=-BY5DR N=10
+make da CAT=-BY5DR N=15
+make ida CAT=-BY5DS N=15
+# make da CAT=-BY5DS N=20
+# make ida CAT=-BY5DR N=20
+# make da CAT=-BY5DR N=25
+# make ida CAT=-BY5DS N=25
+# make da CAT=-BY5DS N=30
+# make ida CAT=-BY5DR N=30
