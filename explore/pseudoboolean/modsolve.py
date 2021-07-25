b9dd321ec9c17173907f3afb84586b44110041d2
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-07-24 22:41:18 -0400

    Experiments with torus & cylinders

--- explore/pseudoboolean/modsolve.py
+++ explore/pseudoboolean/modsolve.py
@@ -5,12 +5,16 @@ import getopt
 
 # Generate and solve embedding of mutilated chessboard problem
 def usage(name):
-    print("Usage: %s [-h] [-v] [-m MOD] [-n N] [-c CORNERS]" % name) 
+    print("Usage: %s [-h] [-v] [-v] [-m MOD] [-n N] [-s SQUARES] [-w hvb" % name) 
     print("  -h         Print this message")
     print("  -v         Run in verbose mode")
+    print("  -u         Stop when cannot find unit pivot")
     print("  -m MOD     Specify modulus")
     print("  -n N       Specify size of board")
-    print("  -c CORNERS Include corners.  String of form CC:CC:..:CC, where CC in {ul, ll, ur, lr}")
+    print("  -s SQUARES Omit squares.")
+    print("             String of form VH:VH:..:VH, where V in {u, m, d} and H in {l, m, r}")
+    print("             Default is 'ul:dr'")
+    print("  -w WRAP    Optionally wrap grid horizontally (h), vertically (v) or both (b)")
 
 
 # General library for solving pseudo-boolean constraints embedded in
@@ -49,7 +53,11 @@ class ModMath:
 
     modulus = 2 # Must be prime
     reciprocals = {} # Dictionary mapping x to 1/x
-    opcount = 0 # Statistics
+    ## Statistics
+    # Number of modular operations
+    opcount = 0
+    # What values get used in places of interest
+    used_values = {} # 
 
     def __init__(self, modulus = 2):
         self.reciprocals = {}
@@ -66,6 +74,7 @@ class ModMath:
                 raise ReciprocalException(x)
         # Reset count
         self.opcount = 0
+        self.used_values = {}
 
     def add(self, x, y):
         self.opcount += 1 
@@ -88,6 +97,17 @@ class ModMath:
         if y == 0:
             raise ZeroException(y)
         return self.mul(x, self.recip(y))
+
+    def mark_used(self, x):
+        self.used_values[x] = True
+        
+    def report_used(self):
+        vlist = sorted(self.used_values.keys())
+        fstring = ", ".join([str(v) for v in vlist])
+        return "{" + fstring + "}"
+
+    def unit_valued(self, x):
+        return x in [0, 1, self.modulus-1]
 
 # Equation of form SUM (a_i * x_i)  =  C
 # Only represent nonzero coefficients
@@ -121,6 +141,7 @@ class Equation:
         return self.nz[i] if i in self.nz else 0
 
     def __setitem__(self, i, v):
+        self.mbox.mark_used(v)
         if v == 0:
             if self.nz:
                 del self.nz[i]
@@ -131,11 +152,19 @@ class Equation:
     def __len__(self):
         return len(self.nz)
 
-    def make_vector(self):
+    def format_dense(self):
         vec = [0 for i in range(self.N)]
         for i in self.nz.keys():
             vec[i] = self.nz[i]
-        return vec + [self.cval]
+        return str(vec + [self.cval])
+
+    def format_sparse(self):
+        slist = []
+        for i in self.nz.keys():
+            v = self.nz[i]
+            slist.append("%d:%d" % (i, v))
+        slist.append("=%d" % self.cval)
+        return '[' + " ".join(slist) + ']'
 
     # Generate new equation from new set of nonzeros
     def spawn(self, nnz, cval):
@@ -165,7 +194,9 @@ class Equation:
     def add(self, other):
         nnz = { i : self.nz[i] for i in self.nz.keys() }
         for i in other.nz.keys():
-            self.nz_insert(nnz, i, self.mbox.add(self[i], other.nz[i]))
+            nx = self.mbox.add(self[i], other.nz[i])
+            self.mbox.mark_used(nx)
+            self.nz_insert(nnz, i, nx)
         nc = self.mbox.add(self.cval, other.cval)
         return self.spawn(nnz, nc)
 
@@ -173,7 +204,9 @@ class Equation:
     def sub(self, other):
         nnz = { i : self.nz[i] for i in self.nz.keys() }
         for i in other.nz.keys():
-            self.nz_insert(nnz, i, self.mbox.sub(self[i], other.nz[i]))
+            nx = self.mbox.sub(self[i], other.nz[i])
+            self.mbox.mark_used(nx)
+            self.nz_insert(nnz, i, nx)
         nc = self.mbox.sub(self.cval, other.cval)
         return self.spawn(nnz, nc)
 
@@ -189,6 +222,7 @@ class Equation:
                 x = self[i]
                 dx = self.mbox.mul(sval, other.nz[i])
                 nx = self.mbox.sub(x, dx)
+                self.mbox.mark_used(nx)
                 self.nz_insert(nnz, i, nx)
         nc = self.mbox.sub(self.cval, self.mbox.mul(sval, other.cval))
         return self.spawn(nnz, nc)
@@ -207,7 +241,10 @@ class Equation:
         return self.cval != 0 and len(self) == 0
 
     def __str__(self):
-        return str(self.make_vector())
+        if self.N <= 40:
+            return self.format_dense()
+        else:
+            return self.format_sparse()
 
 
 # System of equations.
@@ -230,6 +267,8 @@ class EquationSystem:
     rlist = []
 
     ## Accumulating data
+    # List of pivot values
+    pivot_list = []
     # Mapping from variable ID to True
     var_used = {}
     # Total number of equations
@@ -254,6 +293,7 @@ class EquationSystem:
         self.verbose = verbose
         self.mbox = ModMath(modulus)
         self.elist = []
+        self.pivot_list = []
         self.var_used = {}
         self.equation_count = 0
         self.step_count = 0
@@ -306,24 +346,26 @@ class EquationSystem:
 
     # Compute number of columns in remaining rows having nonzero coefficient
     # At position var
-    def column_degree(self, var):
+    def column_degree(self, idx, unit_only):
         d = 0
         for e in self.rlist:
-            if var in e.nz:
-                d += 1
+            if idx in e.nz:
+                if not unit_only or self.mbox.unit_valued(e.nz[idx]):
+                    d += 1
         return d
 
     # Given remaining set of equations, select pivot element
-    def select_pivot(self):
+    def select_pivot(self, unit_only):
         bestI = None
         bestD = 0
         for i in range(self.N):
-            d = self.column_degree(i)
+            d = self.column_degree(i, unit_only)
             if d > 0 and (bestI is None or d < bestD):
                 bestI = i
                 bestD = d
-        self.pivot_degree_sum += bestD
-        self.pivot_degree_max = max(self.pivot_degree_max, bestD)
+        if bestI is not None:
+            self.pivot_degree_sum += bestD
+            self.pivot_degree_max = max(self.pivot_degree_max, bestD)
         return bestI
 
     # Given pivot element, move best equation to top of rlist
@@ -346,18 +388,26 @@ class EquationSystem:
         return bestJ
                 
     # Perform one step of LU decomposition
-    # Return True if decomposition either completed or can't continue
-    def solution_step(self):
+    # Possible return values:
+    # "solved", "unit_stopped", "unsolvable", "normal"
+    def solution_step(self, unit_only):
         if len(self.rlist) == 0:
-            return True
+            return "solved"
         self.step_count += 1
-        pidx = self.select_pivot()
+        pidx = self.select_pivot(True)
         if pidx is None:
-            return True
+            pidx =  self.select_pivot(False)
+            if unit_only:
+                if pidx is not None:
+                    return "unit_stopped"
+        if pidx is None:
+            return "solved" if self.solvable() else "unsolvable"
         j = self.choose_equation(pidx)
         e = self.rlist[0]
+        pval = e[pidx]
+        self.pivot_list.append(pval)
         if self.verbose:
-            print("Pivoting with element %d.  Using row %d" % (pidx, j))
+            print("Pivoting with value %d (element %d).  Using row %d" % (pval, pidx, j))
         self.rlist = self.rlist[1:]
         e = e.normalize(pidx)
         self.slist = self.list_insert(e, self.slist)
@@ -369,24 +419,27 @@ class EquationSystem:
                 self.combine_count += 1
             nrlist.append(re)
         self.rlist = nrlist
-        return False
+        return "normal"
         
             
-    def solve(self):
+    def solve(self, unit_only):
         self.slist = []
         self.rlist = self.elist
         if self.verbose:
             print("Initial state")
             self.show_state()
-
+        status = "normal"
         while True:
-            if self.solution_step():
+            status = self.solution_step(unit_only)
+            # "solved", "unit_stopped", "unsolvable", "normal"
+            if status != "normal":
                 break
             if self.verbose:
                 self.show_state()
         if self.verbose:
-            self.post_statistics()
-        return self.solvable()
+            print("Solution status:%s" % status)
+            self.post_statistics(status)
+        return status
 
     def show(self):
         for e in self.elist:
@@ -399,7 +452,11 @@ class EquationSystem:
         print("Remaining:")
         for e in self.rlist:
             print("   " + str(e))
-        
+
+    def show_remaining_state(self):
+        for e in self.rlist:
+            print("   " + str(e))
+            
     def pre_statistics(self):
         ecount = self.equation_count
         vcount = len(self.var_used)
@@ -407,14 +464,24 @@ class EquationSystem:
         tmax = self.term_max
         print("  Problem: %d equations, %d variables.  %.2f avg vars/equation (max=%d)" % (ecount, vcount, tavg, tmax))
 
-    def post_statistics(self):
-        ok = self.solvable()
-        msg = "System Solvable" if ok else "System Unsolvable"
-        print("  " + msg)
+    def post_statistics(self, status, maybe_solvable):
+        # status: "solved", "unit_stopped", "unsolvable", "normal"
+        expected = "solvable" if maybe_solvable else "unsolvable"
+        print("Solution status: %s (expected = %s)" % (status, expected))
+        if status == "unit_stopped":
+            print("Stopped with remaining equations:")
+            self.show_remaining_state()
         sscount = self.step_count
         pavg = float(self.pivot_degree_sum)/sscount
         pmax = self.pivot_degree_max
         print("  Solving: %d steps.  %.2f avg pivot degree (max=%d)" % (sscount, pavg, pmax))
+        slist = []
+        for s in range(1, len(self.pivot_list)+1):
+            p = self.pivot_list[s-1]
+            if not self.mbox.unit_valued(p):
+                slist.append("%d:%d" % (s,p))
+        if len(slist) > 0:
+            print("  Non-unit pivots: [%s]" % (' '.join(slist)))
         ecount = self.equation_count
         ccount = self.combine_count
         tavg = float(self.term_count)/ecount
@@ -422,7 +489,7 @@ class EquationSystem:
         print("    %d total equations.  %.2f avg vars/equation (max=%d).  %d vector operations" % (ecount, tavg, tmax, ccount))
         sscount = self.step_count
         pavg = float(self.pivot_degree_sum)/sscount
-        print("    %d modular operations" % self.mbox.opcount)
+        print("    %d modular operations.  Used values = %s" % (self.mbox.opcount, self.mbox.report_used()))
 
         
 # Encoding of mutilated chessboard problem as linear equations
@@ -435,41 +502,69 @@ class Board:
     lrvars = {}
     esys = None
 
-    # rcorners is list of squares to exclude.  Can use -1 for rows-1 or cols-1
-    def __init__(self, rows, cols = None, rcorners = ""):
+    # ssquares is string describing squares to remove.  Documented in usage()
+    # Optionally allow grid to wrap horizontally or vertically, giving cylinder or torus
+    def __init__(self, rows, cols, ssquares, wrap_horizontal, wrap_vertical):
         self.rows = rows
-        self.cols = rows if cols is None else cols
-        self.rsquares = self.get_squares(rcorners)
+        self.cols = cols
+        self.rsquares = self.get_squares(ssquares)
+        self.wrap_horizontal = wrap_horizontal
+        self.wrap_vertical = wrap_vertical
         self.udvars = {}
         self.lrars = {}
+
+        print("Wrapping: Horizontal %s.  Vertical %s" % (self.wrap_horizontal, self.wrap_vertical))
+
         # Assign variable IDs
         var = 0
-        for r in range(self.rows-1):
+        rlim = self.rows if wrap_vertical else self.rows-1
+        for r in range(rlim):
             for c in range(self.cols):
                 self.udvars[(r,c)] = var
                 var += 1
+        clim = self.cols if wrap_horizontal else self.cols-1
         for r in range(self.rows):
-            for c in range(self.cols-1):
+            for c in range(clim):
                 self.lrvars[(r,c)] = var
                 var += 1
 
+    # Parse string specifying which strings to omit
     def get_squares(self, rcorners):
         rlist = []
         fields = rcorners.split(":")
         for field in fields:
-            field = field.lower()
             if field == "":
                 continue
-            if field == 'ul':
-                rlist.append((0,0))
-            elif field == 'll':
-                rlist.append((self.rows-1,0))
-            elif field == 'ur':
-                rlist.append((0,self.cols-1))
-            elif field == 'lr':
-                rlist.append((self.rows-1,self.cols-1))
+            field = field.lower()
+            r = 0
+            c = 0
+            ok = True
+            if len(field) != 2:
+                ok = False
             else:
-                raise Exception("Can't parse corner specifier '%s'" % field)
+                lmr = field[1]
+                if lmr == 'l':
+                    c = 0
+                elif lmr == 'm':
+                    c = self.cols/2
+                elif lmr == 'r':
+                    c = self.cols-1
+                else:
+                    ok = False
+
+                umd = field[0]
+                if umd == 'u':
+                    r = 0
+                elif umd == 'm':
+                    r = self.rows/2
+                elif umd == 'd':
+                    r = self.rows-1
+                else:
+                    ok = False
+            if ok:
+                rlist.append((r,c))
+            else:
+                raise Exception("Can't parse square specifier '%s'" % field)
         return rlist
 
 
@@ -479,21 +574,47 @@ class Board:
                 return True
         return False
 
+    def maybe_solvable(self):
+        white_count = 0
+        black_count = 0
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if not self.omit(r,c):
+                    if (r % 2) == (c % 2):
+                        white_count += 1
+                    else:
+                        black_count += 1
+        return abs(white_count - black_count) % 2 == 0
+
     # Return list of variables surrounding given square
     def vars(self, r, c):
         vlist = []
-        if r > 0:
-            vlist.append(self.udvars[(r-1,c)])
-        if r < self.rows-1:
-            vlist.append(self.udvars[(r,c)])
-        if c > 0:
-            vlist.append(self.lrvars[(r,c-1)])
-        if c < self.cols-1:
-            vlist.append(self.lrvars[(r,c)])
+        if self.wrap_vertical:
+            rdown = r-1 if r > 0 else self.rows-1
+            vlist.append(self.udvars[(rdown, c)])
+            rup   = r if r < self.rows else 0
+            vlist.append(self.udvars[(rup, c)])
+        else:
+            if r > 0:
+                vlist.append(self.udvars[(r-1,c)])
+            if r < self.rows-1:
+                vlist.append(self.udvars[(r,c)])
+        if self.wrap_horizontal:
+            cleft = c-1 if c > 0 else self.cols-1
+            vlist.append(self.lrvars[(r, cleft)])
+            cright  = c+1 if c < self.cols-1 else 0
+            vlist.append(self.lrvars[(r, cright)])
+        else:
+            if c > 0:
+                vlist.append(self.lrvars[(r,c-1)])
+            if c < self.cols-1:
+                vlist.append(self.lrvars[(r,c)])
         return vlist
                          
     def equations(self, modulus, verbose):
-        N = self.rows * (self.cols-1) + (self.rows-1) * self.cols
+        rmax = self.rows if self.wrap_vertical else self.rows-1
+        cmax = self.cols if self.wrap_horizontal else self.cols-1
+        N = self.rows * cmax + rmax * self.cols
         esys = EquationSystem(N, modulus, verbose)
         for r in range(self.rows):
             for c in range(self.cols):
@@ -505,44 +626,62 @@ class Board:
                         esys.add_equation(e)
                 else:
                     e = Equation(N, modulus, 1, esys.mbox)
-                    for v in self.vars(r,c):
+                    for v in vars:
                         e[v] = 1
                     esys.add_equation(e)
         return esys
 
         
 
-def solve(verbose, modulus, n, rcorners):
-    b = Board(n, n, rcorners)
+def solve(verbose, modulus, n, ssquares, wrap_horizontal, wrap_vertical, unit_only):
+    b = Board(n, n, ssquares, wrap_horizontal, wrap_vertical)
     esys = b.equations(modulus, verbose)
     if not verbose:
         esys.pre_statistics()
-    solvable = esys.solve()
+    status = esys.solve(unit_only)
     if not verbose:
-        esys.post_statistics()
+        esys.post_statistics(status, b.maybe_solvable())
 
 def run(name, args):
     verbose = False
+    unit_only = False
     modulus = 3
     n = 8
-    rcorners = "ul:lr"
-    optlist, args = getopt.getopt(args, "hvm:n:c:")
+    ssquares = "ul:dr"
+    wrap_horizontal = False
+    wrap_vertical = False
+    optlist, args = getopt.getopt(args, "hvum:n:s:w:")
     for (opt, val) in optlist:
         if opt == '-h':
             usage(name)
             return
         elif opt == '-v':
             verbose = True
+        elif opt == '-u':
+            unit_only = True
         elif opt == '-m':
             modulus = int(val)
         elif opt == '-n':
             n = int(val)
-        elif opt == '-c':
-            rcorners = val
-    ocorners = rcorners.split(":")
+        elif opt == '-s':
+            ssquares = val
+        elif opt == '-v':
+            verbose = True
+        elif opt == '-w':
+            ok = False
+            if val in "hb":
+                wrap_horizontal = True
+                ok = True
+            if val in "vb":
+                ok = True
+                wrap_vertical = True
+            if not ok:
+                print("Invalid wrapping parameter '%s'.  Must be h, v, or b" % val)
+                return
+    ocorners = ssquares.split(":")
     scorners = '[' + ", ".join(ocorners) + ']'
-    print("N = %d.  Modulus = %d.  Omitting corners %s" % (n, modulus, scorners))
-    solve(verbose, modulus, n, rcorners)
+    print("N = %d.  Modulus = %d.  Omitting squares %s" % (n, modulus, scorners))
+    solve(verbose, modulus, n, ssquares, wrap_horizontal, wrap_vertical, unit_only)
 
 if __name__ == "__main__":
     run(sys.argv[0], sys.argv[1:])
