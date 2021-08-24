c295bfb07a390813ac84f25a1be952aa7797f37f
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-08-24 07:47:22 -0400

    More benchmarks

---
+++ benchmarks/data-lrat/pigeon-tseitin-data/gen-constraint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+make con N=004
+make con N=008
+make con N=012
+make con N=016
+make con N=020
+make con N=024
+make con N=032
+make con N=040
+make con N=048
+make con N=056
+make con N=064
+#make con N=068
+#make con N=072
+#make con N=076
+#make con N=080
+#make con N=096
+#make con N=112
+#make con N=128
+#make con N=144
+#make con N=150
+#make con N=152
+#make con N=160
