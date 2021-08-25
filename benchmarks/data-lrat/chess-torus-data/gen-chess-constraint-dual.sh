d98871bcdd7e49a175da838de45137c78969a7a4
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2021-08-24 20:45:41 -0400

    More benchmarks

---
+++ benchmarks/data-lrat/chess-torus-data/gen-chess-constraint-dual.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+make dc N=004
+make dc N=008
+make dc N=012
+make dc N=016
+make dc N=020
+make dc N=024
+make dc N=028
+make dc N=032
+make dc N=040
+make dc N=048
+make dc N=056
+make dc N=064
+#make dc N=080
+#make dc N=096
+#make dc N=112
+#make dc N=128
+#make dc N=144
+#make dc N=160
+
