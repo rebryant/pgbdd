00df4a5fde1df9a23b9939b17891fbcd0e173ddf
Randy Bryant <bryant@BRYANT-MC2.VLSI.CS.CMU.EDU>
2021-09-13 22:02:29 -0400

    Tune benchmarks

--- benchmarks/data-lrat/pigeon-direct-data/gen-direct-constraint.sh
+++ benchmarks/data-lrat/pigeon-direct-data/gen-direct-constraint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 make pc N=004
 make pc N=008
 make pc N=012
@@ -7,5 +7,5 @@ make pc N=020
 make pc N=024
 make pc N=028
 make pc N=032
-make pc N=036
-make pc N=040
+#make pc N=036
+#make pc N=040
